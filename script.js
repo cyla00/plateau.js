a21c5f9a6034045a2fe0658055366fe0f57a502a
cyla00 <ikhayam000@protonmail.com>
2020-06-18 10:15:28 +0200

    script

--- script.js
+++ script.js
@@ -1,67 +1,46 @@
-var size = 19;
-var box = document.getElementsByClassName("plateau")[0]
-for(var wx = 1; wx < size; wx++)
-{
-  for(var wy = 1; wy < size; wy++)
-  {
-    if(wx % 2 == 1 && wy % 2 == 1)
-    {
-        var newwall = document.createElement("div"), ws = newwall.style;
+//var plateau = document.getElementById('plateau'); // variables objet
+var pion = document.getElementById('pion');
+var x;
+var y;
 
-        newwall.classList.add("mur");
-        ws.left = String(wx * 40)+'px';
-        ws.top = String(wy * 40)+'px';
+// programmation des touches de direction
+document.onkeydown = function(event) {
+  if (event.keyCode == 37) gauche();
+  if (event.keyCode == 39) droite();
+  if (event.keyCode == 38) haut();
+  if (event.keyCode == 40) bas();
+} // fin fonction
 
-
-        box.appendChild(newwall);
-    }
-  }
+// quatre fonctions de déplacement
+function gauche() {
+  x = getComputedStyle(pion).left;
+  x = parseInt(x);
+  x = x - 40 + "px";
+  pion.style.left = x;
 }
-var quadrato = document.getElementsByClassName('quadrato'),
-    s = quadrato.style; // Un petit raccourci
-var x = 0;
-var y = 0;
 
-
-document.onkeydown = function(event){
-    var nx = x, ny = y;
-    var event = event || window.event,
-        keyCode = event.keyCode;
-
-    // On détecte l'événement puis selon la fleche, on ajoute le nombres de pixels désiré (ici 3) aux valeurs globales de position, x et y.
-    switch(keyCode){
-    case 90:
-        ny = ny - 1;
-        break;
-
-    case 83:
-        ny = ny + 1;
-        break;
-
-    case 81:
-        nx = nx - 1;
-        break;
-
-    case 68:
-        nx = nx + 1;
-        break;
-    }
-
-   // On vérifie si les valeurs sont supérieures ou égales à 0 et inférieures et égal à 760
-   if(nx % 2 == 1 && ny % 2 == 1)
-    {
-      return;
-    }
-    x = nx, y = ny;
-
-    if (x < 0) x = 0; // Si elles sont inférieures à 0
-    if (y < 0) y = 0;
-    if (x > (size - 1)) x = (size - 1);// Si elles sont supérieures à 40
-    if (y > (size - 1)) y = (size - 1);
-
-
-    // Et enfin on applique les modifications :
-    s.left = String(x * 40)+'px';
-    s.top = String(y * 40)+'px';
-
+function droite() {
+  x = getComputedStyle(pion).left;
+  x = parseInt(x);
+  x = x + 40 + "px";
+  pion.style.left = x;
 }
+
+function haut() {
+  y = getComputedStyle(pion).top;
+  y = parseInt(y);
+  y = y - 40 + "px";
+  pion.style.top = y;
+}
+
+function bas() {
+  y = getComputedStyle(pion).top;
+  y = parseInt(y);
+  y = y + 40 + "px";
+  pion.style.top = y;
+}
+
+if (x < 0 + "px") x = 0 + "px"; // Si elles sont inférieures à 0
+if (y < 0 + "px") y = 0 + "px";
+if (x > 760 + "px") x = 760 + "px";// Si elles sont supérieures à 760
+if (y > 760 + "px") y = 760 + "px";
